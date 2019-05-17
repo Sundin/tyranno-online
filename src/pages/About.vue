0aae6bdb12336807b7871cec193f5bbb1748200d
Gustav Sundin <sundin666@gmail.com>
2019-05-17 16:37:43 +0200

    Content

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,24 @@
 <template>
   <main-layout>
-    <p>Hyper Speed Metal</p>
+    <p>Hyper Speed Metal from Hisingen Island in Gothenburg, Sweden.</p>
+    <p>Contact: armoryofficial@gmail.com</p>
+    <h1>Lineup</h1>
+    <ul>
+      <li>Anglegrinder – Thunder Bass</li>
+      <li>Konstapel P. – Vocals</li>
+      <li>Space Ace – Drums</li>
+      <li>G.G. Sundin – Guitars</li>
+      <li>Ingelman – Guitars</li>
+    </ul>
+    <img src="../assets/band_photo.jpg">
+    <h1>Discography</h1>
+    <ul>
+      <li>2013: Open Fire! (Demo)</li>
+      <li>2015: S.M.I. (Demo)</li>
+      <li>2016: World Peace... Cosmic War</li>
+      <li>2018: The Search</li>
+      <li>2018: Live in the Abyss (Live Cassette)</li>
+    </ul>
   </main-layout>
 </template>
 
@@ -12,3 +30,9 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+}
+</style>
--- src/pages/Merch.vue
+++ src/pages/Merch.vue
@@ -1,8 +1,12 @@
 <template>
   <main-layout>
     <h1>Merch</h1>
+    <p>Support Armory by ordering your speed metal merchandise from any of the following links.</p>
     <p>
       <a href="https://www.jawbreaker.se">Jawbreaker Records</a>
+    </p>
+    <p>
+      <a href="https://armory417.bandcamp.com/">Bandcamp Page</a>
     </p>
   </main-layout>
 </template>
