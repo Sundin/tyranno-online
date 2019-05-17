074c487d8b963d35634fd68ff7cf729a889a241d
Gustav Sundin <sundin666@gmail.com>
2019-05-17 17:03:53 +0200

    Past gigs

--- src/pages/Tour.vue
+++ src/pages/Tour.vue
@@ -4,6 +4,32 @@
     <p>Noselake</p>
     <p>Metal Magic Festival</p>
     <h1>Past Gigs</h1>
+    <p>2018-10-20: Plan B, Malmö, Sweden</p>
+    <p>2018-07-28: Truckstop Alaska, Gothenburg, Sweden (releaseparty for The Search)</p>
+    <p>2018-06-02: Muskelrock, Blädinge, Sweden</p>
+    <p>2018-03-03: Turbofest, Ljungby, Sweden</p>
+    <p>2017-09-29: Huskvarna Rock & Art, Huskvarna, Sweden</p>
+    <p>2017-06-16: The Abyss, Gothenburg, Sweden</p>
+    <p>2017-06-10: Truckstop Alaska, Gothenburg, Sweden</p>
+    <p>2017-04-29: Gothenburg Rockfest, Sweden</p>
+    <p>2017-02-18: Klubb Ostronet, Gothenburg, Sweden</p>
+    <p>2017-02-17: Club Abraxas, Malmö, Sweden</p>
+    <p>2016-11-19: Truckstop Alaska, Gothenburg, Sweden</p>
+    <p>2016-09-03: Copperfields, Stockholm, Sweden</p>
+    <p>2016-08-26: Truckstop Alaska, Gothenburg, Sweden (releaseparty for World Peace… Cosmic War)</p>
+    <p>2016-05-21: Truckstop Alaska, Gothenburg, Sweden (releaseparty for Critical Mass Vol. 3)</p>
+    <p>2016-03-12: Pokalen, Oslo, Norway</p>
+    <p>2015-10-31: Belsepub, Gothenburg, Sweden</p>
+    <p>2015-09-12: Blood & Meat Fest, Vedum, Sweden</p>
+    <p>2015-06-27: Truckstop Alaska, Gothenburg, Sweden</p>
+    <p>2015-05-16: Röde Orm biker’s club, Götene, Sweden (secret gig)</p>
+    <p>2014-09-27: Belsepub, Gothenburg, Sweden</p>
+    <p>2014-05-07: Sticky Fingers, Gothenburg, Sweden</p>
+    <p>2014-03-29: Kryptan, Växjö, Sweden</p>
+    <p>2013-04-27: Brygghuset, Borås, Sweden</p>
+    <p>2012-10-13: Verket, Umeå, Sweden</p>
+    <p>2012-10-12: Sliperiet, Örnsköldsvik, Sweden</p>
+    <p>2012-10-06: Stenhuset, Gothenburg, Sweden</p>
   </main-layout>
 </template>
 
