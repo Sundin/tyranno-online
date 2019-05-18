d33a573177f467970780f154f5f8c361a94234f0
Gustav Sundin <sundin666@gmail.com>
2019-05-18 17:32:37 +0200

    Add press kit

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -3,6 +3,23 @@
     <h1>About Armory</h1>
     <p>Hyper Speed Metal from Hisingen Island in Gothenburg, Sweden.</p>
     <p>Contact: armoryofficial@gmail.com</p>
+
+    <p>
+      Armory started out in 2012 and has been delivering hyperspeed metal since. After two
+      demos the debut full length album World Peace… Cosmic War was released via High
+      Roller Records in 2016. The album got great reviews from all around the world and was
+      priced “Album of the year” by Metal Invader. The band then got to share the stage with
+      bigger bands such as Pentagram and Nifelheim. With all the new experience the band
+      went into the studio again in late 2017 to record their second album. The Search was
+      released in July 2018 via High Roller and again the album got great reviews and it was
+      proved that the songwriting had developed in the right direction. Today Armory are
+      aiming to bring the band to the next level and to deliver their hypespeed metal outside
+      Scandinavia.
+    </p>
+
+    <p>
+      <a href="/press_kit.zip" download>Download press kit</a>
+    </p>
     <h1>Lineup</h1>
 
     <p>Anglegrinder – Thunder Bass</p>
@@ -24,10 +41,27 @@
       <a href="https://armory417.bandcamp.com/">Bandcamp</a>
     </p>
     <p>
+      <a
+        href="https://open.spotify.com/artist/2mAu2bKoNDxD1jybBS1rku?si=PdF2nwUIRpam5CRc7V3i9Q"
+      >Spotify</a>
+    </p>
+    <p>
+      <a href="https://youtu.be/UYj3EnEON8k">Vault Seven official video</a>
+    </p>
+    <p>
+      <a href="https://www.youtube.com/watch?v=hTNFsTV38hA&t=80s">Hyperion lyric video</a>
+    </p>
+    <p>
+      <a href="https://www.youtube.com/watch?v=9YDNAMrtTVU">The Search album trailer</a>
+    </p>
+    <p>
       <a href="https://www.metal-archives.com/bands/Armory/3540372230">Metal Archives</a>
     </p>
     <p>
       <a href="https://www.facebook.com/armoryofficial/">Facebook</a>
+    </p>
+    <p>
+      <a href="https://www.instagram.com/armorysweden/">Instagram</a>
     </p>
   </main-layout>
 </template>
