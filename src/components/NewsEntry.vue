8608a92990088854bb4c4663a763234fa816510f
Gustav Sundin <sundin666@gmail.com>
2019-05-18 16:13:06 +0200

    Adds dynamic NewsEntry component

---
+++ src/components/NewsEntry.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="news-entry">
+    <h3>{{item.heading}}</h3>
+    <p>
+      {{item.date}}:
+      <span v-html="item.body"></span>
+    </p>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NewsEntry",
+  props: {
+    item: Object
+  }
+};
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,25 +1,39 @@
 <template>
   <main-layout>
     <h1>News</h1>
-    <h3>Armory Online</h3>
-    <p>2019-05-17: Website launched.</p>
-    <hr>
-    <h3>Music Video</h3>
-    <p>
-      2018-10-04: Our first music video is out now! The song is "Vault Seven" from our latest album "The Search", out now through High Roller Records.
-      The video was directed by the mighty Jim Nedergård and is a dark story about invisable enemies, paranoia and the dangerous power of knowledge.
-      <a
-        href="https://www.youtube.com/watch?v=UYj3EnEON8k"
-      >Watch it here!</a>
-    </p>
+    <div v-for="item in getNewsEntries()">
+      <news-entry v-bind:item="item"></news-entry>
+    </div>
   </main-layout>
 </template>
 
 <script>
+const news = [
+  {
+    heading: "Armory Online",
+    date: "2019-05-17",
+    body: "Website launched."
+  },
+  {
+    heading: "Music Video",
+    date: "2018-10-04",
+    body: `Our first music video is out now! The song is "Vault Seven" from our latest album "The Search", out now through High Roller Records.
+      The video was directed by the mighty Jim Nedergård and is a dark story about invisable enemies, paranoia and the dangerous power of knowledge.
+     <a href="https://www.youtube.com/watch?v=UYj3EnEON8k">Watch it here!</a>`
+  }
+];
+
 import MainLayout from "../layouts/Main.vue";
+import NewsEntry from "../components/NewsEntry.vue";
 export default {
   components: {
-    MainLayout
+    MainLayout,
+    NewsEntry
+  },
+  methods: {
+    getNewsEntries: function() {
+      return news;
+    }
   }
 };
 </script>
