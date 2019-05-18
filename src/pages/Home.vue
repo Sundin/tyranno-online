17ed0b82007d0d51110bdec30ab161069f85dcbd
Gustav Sundin <sundin666@gmail.com>
2019-05-18 15:28:13 +0200

    News section

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,16 @@
 <template>
   <main-layout>
-    <h1>Armory Online</h1>
-    <p>Hyper Speed Metal from Space!</p>
+    <h1>News</h1>
+    <h3>Armory Online</h3>
+    <p>2019-05-17: Website launched.</p>
+    <h3>Music Video</h3>
+    <p>
+      2018-10-04: Our first music video is out now! The song is "Vault Seven" from our latest album "The Search", out now through High Roller Records.
+      The video was directed by the mighty Jim Nedergård and is a dark story about invisable enemies, paranoia and the dangerous power of knowledge.
+      <a
+        href="https://www.youtube.com/watch?v=UYj3EnEON8k"
+      >Watch it here!</a>
+    </p>
   </main-layout>
 </template>
 
