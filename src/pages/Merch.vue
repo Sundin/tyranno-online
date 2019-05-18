4544fa34e6b6115a4bf96c30d298eb39b2d6a929
Gustav Sundin <sundin666@gmail.com>
2019-05-18 15:21:09 +0200

    Link to HRR

--- src/pages/Merch.vue
+++ src/pages/Merch.vue
@@ -1,12 +1,16 @@
 <template>
   <main-layout>
     <h1>Merch</h1>
-    <p>Support Armory by ordering your speed metal merchandise from any of the following links.</p>
+    <p>Support Armory by ordering your speed metal merchandise from any of the following links. All money goes directly to the band.</p>
     <p>
-      <a href="https://www.jawbreaker.se">Jawbreaker Records</a>
+      <a href="https://www.jawbreaker.se/product-tag/armory/">Jawbreaker Records</a>
     </p>
     <p>
       <a href="https://armory417.bandcamp.com/">Bandcamp Page</a>
+    </p>
+    <p>For vinyls, please go to High Roller Records:</p>
+    <p>
+      <a href="https://www.hrrshop.de/navi.php?suchausdruck=armory">High Roller Records Webshop</a>
     </p>
   </main-layout>
 </template>
