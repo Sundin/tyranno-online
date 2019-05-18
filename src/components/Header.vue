8d5c56d25b5a6bdb72ad8a523e2196b7b8740677
Gustav Sundin <sundin666@gmail.com>
2019-05-18 11:05:41 +0200

    Responsive menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,15 +3,13 @@
     <v-link href="/">
       <img alt="Armory logo" src="../assets/logo.png">
     </v-link>
-    <ul>
-      <li>
-        <v-link href="/">Home</v-link>
-        <v-link href="/tour">Tour</v-link>
-        <v-link href="/merch">Merch</v-link>
-        <v-link href="/fanclub">Fanclub</v-link>
-        <v-link href="/about">About</v-link>
-      </li>
-    </ul>
+    <div class="navbar">
+      <v-link href="/">Home</v-link>
+      <v-link href="/tour">Tour</v-link>
+      <v-link href="/merch">Merch</v-link>
+      <v-link href="/fanclub">Fanclub</v-link>
+      <v-link href="/about">About</v-link>
+    </div>
   </div>
 </template>
 
@@ -32,20 +30,26 @@ img {
   max-width: 100%;
   height: auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.navbar {
+  width: 100%;
+  overflow: auto;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+@media screen and (max-width: 500px) {
+  .navbar a {
+    float: none;
+    display: block;
+  }
 }
+
 a {
   color: var(--text-color);
   padding: 0 20px;
   font-family: Impact, Charcoal, sans-serif;
   font-size: 30px;
 }
+
 a.active {
   color: var(--link-color);
 }
