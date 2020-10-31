e94176487d7568e1accd7911730445d80e532b44
Gustav Sundin <sundin666@gmail.com>
2020-10-31 12:04:16 +0100

    Void Prison

--- src/pages/Vault.vue
+++ src/pages/Vault.vue
@@ -2,7 +2,30 @@
   <main-layout>
     <div v-if="hasAccess">
       <h1>Greetings Space Marauder!</h1>
-      <h2>2020</h2>
+      <h2>2020 - part 2</h2>
+      <p>
+        Hello everybody, Armory are back! As you might already know,
+        we have been quarantined onboard our space ship for the last six months,
+        writing a new concept album.
+        The writing process has now been finished and recording will commence during this winter.
+        Some demos have already been recorded in our rehearsal pod,
+        and we would like to break the silence by sharing one of these demo tracks with you,
+        our faithful Space Marauders!
+      </p>
+      <p>
+        So behold the "VOID PRISON", one of the new tracks and our most daring song to date.
+        Bear in mind that this is a demo recording that will be re-recorded for the album.
+      </p>
+      <p>
+        Enjoy, and let us know what you think!
+        Until the next time - have a good sin.
+      </p>
+      <br />
+      <audio controls>
+        <source src="../assets/Void_Prison_Demo.mp3" type="audio/mpeg" controls />
+        Your browser does not support the audio element.
+      </audio>
+      <h2>2020 - part 1</h2>
       <p>
       The membership card of 2020 is the circuit board from an actual UFO detector.
       You will find the instructions on how to use it to contruct your very own UFO detector here:
