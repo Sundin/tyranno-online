578b0e617127ba286699ab8887b0c6669062c310
Gustav Sundin <sundin666@gmail.com>
2022-04-28 08:45:34 +0200

    Clickable image

--- src/home/Home.vue
+++ src/home/Home.vue
@@ -3,8 +3,13 @@
     <!-- <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> -->
     <h1>Katakombernas Kakofoni</h1>
     <p>Tyrannosatan's ripping debut LP.</p>
+    <p>
+      Order from <a href="https://www.jawbreaker.se/product-tag/tyrannosatan/" target="_blank">Jawbreaker Records</a>!
+    </p>
     <h2>Out Now!</h2>
-    <img alt="Katekombernas Kakofoni" src="../assets/tyrannosatan katakombernas kakofoni.png"/>
+    <a href="https://www.jawbreaker.se/product-tag/tyrannosatan/" target="_blank">
+      <img alt="Katekombernas Kakofoni" src="../assets/tyrannosatan katakombernas kakofoni.png"/>
+    </a>
     <h2>What does the fans have to say?</h2>
     <p>"It's a thunderous, raw and absolute ripper of an album and an instant favourite of mine!!"
       - Paul Holland, UK</p>
