1e3f9be26a151df8d96dfe83bb4a4aa9c95b8f9b
Gustav Sundin <sundin666@gmail.com>
2022-04-28 10:12:17 +0200

    More quotes

--- src/home/Home.vue
+++ src/home/Home.vue
@@ -8,12 +8,23 @@
     </p>
     <h2>Out Now!</h2>
     <a href="https://www.jawbreaker.se/product-tag/tyrannosatan/" target="_blank">
-      <img alt="Katekombernas Kakofoni" src="../assets/tyrannosatan katakombernas kakofoni.png"/>
+      <img alt="Katekombernas Kakofoni" src="../assets/tyrannosatan katakombernas kakofoni.png" />
     </a>
     <h2>What does the fans have to say?</h2>
     <p>"It's a thunderous, raw and absolute ripper of an album and an instant favourite of mine!!"
       - Paul Holland, UK</p>
-    <p>"Your band sounds good, a mix of styles in a METAL way!" - Jorge / Mina de Aço</p>
+    <p>"A completely bonkers album that I pickep up at Mephisto
+      [Copenhagen record store] a few days ago.
+      A vile piece
+      of black metal with maces, morning stars, swords,
+      axes and all sorts of weapons swung above the head. And it's
+      sung in Swedish which nudges the coolness of this album to the roof!"
+      - tr00makrel, Denmark</p>
+    <p>"It's early in the year but this is one of my favourites already -
+      raw, thunderous and old school! Highly
+      recommend getting a copy at jawbreaker.se!" - __deathdigital__</p>
+    <p>"Your band sounds good, a mix of styles in a METAL way!" - Jorge / Mina de Aço, Portugal</p>
+    <p>"Unrelenting blackend death thrash!!" - Chris, Mercenary Press / Inhuman Nature, UK</p>
     <p></p>
   </main-layout>
 </template>
